2482e55d5604913027a57862fd7f3638f7f400b4
log1ri <feldekper@gmail.com>
2025-12-15 19:11:22 +0700

    modifile ocr route

--- app/routers/ocr.py
+++ app/routers/ocr.py
@@ -1,18 +1,17 @@
 from fastapi import APIRouter, HTTPException, Depends
 from fastapi.concurrency import run_in_threadpool
 from datetime import datetime
+from app.core.config import get_settings 
 from app.services.ocr_service import OCRService
 from app.services.ocr_mongo_service import OcrMongoService
 from app.services.do_space import DOService
 from app.schemas.ocr import ImgBody 
 from functools import lru_cache
 
-
+settings = get_settings()
 class BusinessLogicError(Exception):
     pass
 
-class OCRServiceError(Exception):
-    pass
 
 class StorageServiceError(Exception):
     pass
@@ -26,7 +25,7 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-service = DOService()
+do_service = DOService()
 mongo_service = OcrMongoService()
 ocr_service_instance = OCRService()
 def get_ocr_service():
@@ -41,17 +40,66 @@ def get_ocr_service():
 async def predict(payload: ImgBody, ocr_service: OCRService = Depends(get_ocr_service)):
     try:
         result =  await run_in_threadpool(ocr_service.predict, payload.imgBase64)
+        ocr_data = {
+            "regNum": result.get("regNum"),
+            "province": result.get("province"),
+            "confidence": result.get("confidence"),
+            "readStatus": result.get("readStatus"),
+        }
+
+        image_bytes = {
+            "originalImage": result.get("originalImage"),
+            "croppedPlateImage": result.get("croppedPlateImage"),
+        }
+        
         organization = await mongo_service.mapCamId(payload.camId)
-        if not organization:
+        subId = await mongo_service.get_UID_by_organize(organization)
+        
+        if not organization or not subId:
             raise BusinessLogicError(f"Organization for Camera ID '{payload.camId}' not found")
+        
+        
+        now = datetime.utcnow().strftime("%Y%m%d-%H%M%S-%f")
+        orig_path = f"{settings.ORI_IMG_LOG_PATH_PREFIX}/{now}.jpg".replace("subId", subId)
+        crop_path = f"{settings.PRO_IMG_LOG_PATH_PREFIX}/{now}.jpg".replace("subId", subId)
+        issue_pro_path = f"{settings.ISSUE_LOG_PATH_PREFIX}/{now}.jpg".replace("subId", subId)
+        
         if result.get("readStatus") == "complete":
-            db = await mongo_service.log_ocr(result, organization)
+            
+            url = await do_service.upload_two_images(image_bytes.get("originalImage"), image_bytes.get("croppedPlateImage"), orig_path, crop_path)
+            # db = await mongo_service.log_ocr(ocr_data, organization)
         else:
-            db = None
+            orig_path = f"{settings.ISSUE_LOG_PATH_PREFIX}/{now}.jpg".replace("subId", subId)
+            print("orig_path:", orig_path)
+            url = await do_service.upload_image(image_bytes.get("originalImage"), orig_path, content_type="image/jpeg")
+            # db = None
+            
+        match result.get("readStatus"):
+            case "complete":
+
+                url = await do_service.upload_two_images(
+                    image_bytes.get("originalImage"), 
+                    image_bytes.get("croppedPlateImage"), 
+                    orig_path, 
+                    crop_path)
+                
+            case "no_text":
+                url = await do_service.upload_image(
+                    image_bytes.get("croppedPlateImage"), 
+                    issue_pro_path, 
+                    content_type="image/jpeg")
+
+            case "no_plate":
+                url = await do_service.upload_image(
+                    image_bytes.get("originalImage"), 
+                    issue_pro_path, 
+                    content_type="image/jpeg")
+               
         
         return {
-            "ocr-response": result, 
-            "log": db
+            "ocr-response": ocr_data, 
+            "do-service": url
+            # "log": db
         }  
     
     except BusinessLogicError as e:
